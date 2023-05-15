a249f2219ba6de3e836809104eab992e3545da87
BenLasanas <benedictlasanas@gmail.com>
2023-05-15 10:58:54 +0800

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import SayCard from "./components/SayCard";
+
+function App() {
+  return (
+    <div>
+      <SayCard />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/SayCard.tsx
@@ -0,0 +1,10 @@
+function SayCard() {
+  return (
+    <div>
+      <h2>Weather</h2>
+      <h2>Time</h2>
+    </div>
+  );
+}
+
+export default SayCard;
